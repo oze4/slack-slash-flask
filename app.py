b1e74894d421ddc35817727fc2fcb8e61b45e932
Matt Oestreich <matthewpoestreich@gmail.com>
2019-11-13 16:00:50 -0600

    Update

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, Response
-import json, env_settings, os
+import json, env_settings, os, dotenv
 import request_helper
 
+dotenv.load_dotenv(verbose=True)
 
-app = Flask(__name__)
 
 HOST = "0.0.0.0"
 PORT = os.getenv("PORT")
 DEBUG = True
 
+app = Flask(__name__)
 
 @app.route("/slash/flask", methods=['POST'])
 def slash_flask():
     try:
         validated = request_helper.validate_request(request)
-        return validated
-        if validated == True:
-            return "[Python:Flask]::Success :smile: "
+        #return validated
+        if type(validated) is bool:
+            if validated == True:
+                return "[Python:Flask]::Success :smile: "
+            else:
+                return Response('<Unable to validate request>', 401, {'x-no-validation':'validation="unable"'})
         else:
-            return Response('<Unable to validate request>', 401, {'x-no-validation':'validation="unable"'})
+            return validated.json()
     except Exception as e:
         print("***ERROR***")
         print(e)
         print("***ERROR***")
-        return "Error :cry: "
+        return "Error :cry: " + e
 
 
 if __name__ == '__main__':
